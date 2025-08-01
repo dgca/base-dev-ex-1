28cd26a61332583d43dcce7cd082d895ded8204b
Dan Cortes <3639170+dgca@users.noreply.github.com>
2025-08-01 15:33:04 -0600

    Fix

--- minikit.config.ts
+++ minikit.config.ts
@@ -9,9 +9,10 @@ export const minikitConfig = {
   accountAssociation: {
     header:
       "eyJmaWQiOjYxNjIsInR5cGUiOiJhdXRoIiwia2V5IjoiMHg1MmE0NTdCZGU1NGZERWFGNzIxOTlBQTZhNjJENUMwMjdiQzhDYmQ4In0",
-    payload: "eyJkb21haW4iOiJiYXNlLWRldi1leC0xLnZlcmNlbC5hcHAifQ",
+    payload:
+      "eyJkb21haW4iOiJiYXNlLWRldi1leC0xLWdpdC11YXQtMDctdHlwZW9mLXByb2plY3Qtdmlldy52ZXJjZWwuYXBwIn0",
     signature:
-      "ddNa80RaK59B3uqotGCojZACBbRlZCZu5MnDRjbQA/5qp+rJs+a1BOy0aYKMEAdiCzoYUQ71iojeLz/RIJssdRw=",
+      "W1YRNhxOMBgGrsWjse09bAyngRFDZUsD4hoguCMRScwW+DcbHAVpgbn1tH+BXGfEAm/8rO3fqurvkUWFuIGoBxs=",
   },
   frame: {
     version: "1",
